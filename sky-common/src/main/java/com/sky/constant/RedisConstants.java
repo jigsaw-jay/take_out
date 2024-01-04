c66f9cd90b07d62955205e83586d0a5af904a533
jigsaw <jigsaw@jay.cn>
2024-01-04 16:24:34 +0800

    Shop状态管理

---
+++ sky-common/src/main/java/com/sky/constant/RedisConstants.java
@@ -0,0 +1,5 @@
+package com.sky.constant;
+
+public class RedisConstants {
+    public static final String SHOP_STATUS_KEY = "shop:status:";
+}
--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -1,17 +1,45 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.RedisConstants;
+import com.sky.context.BaseContext;
+import com.sky.result.Result;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/shop")
+@RequestMapping("/admin/shop")
 @Slf4j
 @Tag(name = "Shop")
 public class ShopController {
-//    @GetMapping("/status")
-//    @Operation("")
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 设置营业状态
+     *
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @Operation(summary = "设置营业状态")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("设置营业状态为:{}", status == 1 ? "营业中" : "打烊");
+        stringRedisTemplate.opsForValue().set(RedisConstants.SHOP_STATUS_KEY, status.toString());
+        return Result.success();
+    }
+
+    /**
+     * 查询营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @Operation(summary = "获取营业状态|")
+    public Result<Integer> getStatus() {
+        int status = Integer.parseInt(stringRedisTemplate.opsForValue().get(RedisConstants.SHOP_STATUS_KEY));
+        return Result.success(status);
+    }
 }
